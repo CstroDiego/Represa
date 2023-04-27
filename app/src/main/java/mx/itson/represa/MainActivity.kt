4d8c37a30a01f52fbddb1e7cbde8baeb5ccd3e4a
Diego Castro <diego.castro@potros.itson.edu.mx>
2023-04-27 01:44:45 -0700

    V1.0

--- app/src/main/java/mx/itson/represa/MainActivity.kt
+++ app/src/main/java/mx/itson/represa/MainActivity.kt
@@ -2,12 +2,11 @@ package mx.itson.represa
 
 import android.content.Intent
 import android.graphics.Color
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import cn.pedant.SweetAlert.SweetAlertDialog
 import com.ingenieriajhr.blujhr.BluJhr
 import com.jjoe64.graphview.series.DataPoint
@@ -25,23 +24,25 @@ class MainActivity : AppCompatActivity() {
 
     //graphviewSeries
     lateinit var temperatura: LineGraphSeries<DataPoint?>
-    lateinit var humedad:LineGraphSeries<DataPoint>
+    lateinit var humedad: LineGraphSeries<DataPoint>
 
     //nos indica si estamos recibiendo datos o no
     var initGraph = false
+
     //nos almacena el estado actual de la conexion bluetooth
     var stateConn = BluJhr.Connected.False
 
     //valor que se suma al eje x despues de cada actualizacion
-    var ejeX = 0.6
+    var ejeX = 0.5
 
     //sweet alert necesarios
-    lateinit var loadSweet : SweetAlertDialog
-    lateinit var errorSweet : SweetAlertDialog
-    lateinit var okSweet : SweetAlertDialog
-    lateinit var disconnection : SweetAlertDialog
+    lateinit var loadSweet: SweetAlertDialog
+    lateinit var errorSweet: SweetAlertDialog
+    lateinit var okSweet: SweetAlertDialog
+    lateinit var disconnection: SweetAlertDialog
 
     private lateinit var binding: ActivityMainBinding
+    private val REQUEST_ENABLE_BLUETOOTH = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,13 +50,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
         //graphview
         initGraph()
+
         //init var sweetAlert
         initSweet()
 
         blue = BluJhr(this)
         blue.onBluetooth()
+
+        // *****************************************************************************************
+        blue.initializeBluetooth()
+        // *****************************************************************************************
 
         binding.btnViewDevice.setOnClickListener {
             when (graphviewVisible) {
@@ -67,7 +74,6 @@ class MainActivity : AppCompatActivity() {
         binding.listDeviceBluetooth.setOnItemClickListener { adapterView, view, i, l ->
             if (devicesBluetooth.isNotEmpty()) {
                 blue.connect(devicesBluetooth[i])
-                //genera error si no se vuelve a iniciar los objetos sweet
                 initSweet()
                 blue.setDataLoadFinishedListener(object : BluJhr.ConnectedBluetooth {
                     override fun onConnectState(state: BluJhr.Connected) {
@@ -94,24 +100,21 @@ class MainActivity : AppCompatActivity() {
                                 disconnection.show()
                                 visibleListDevice()
                             }
-
                         }
                     }
                 })
             }
         }
-
-
-
         binding.btnInitStop.setOnClickListener {
-            if (stateConn == BluJhr.Connected.True){
-                initGraph = when(initGraph){
-                    true->{
+            if (stateConn == BluJhr.Connected.True) {
+                initGraph = when (initGraph) {
+                    true -> {
                         blue.bluTx("0")
                         binding.btnInitStop.text = "INICIAR"
                         false
                     }
-                    false->{
+
+                    false -> {
                         blue.bluTx("1")
                         binding.btnInitStop.text = "DETENER"
                         true
@@ -119,46 +122,41 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
     }
 
     private fun rxReceived() {
-        blue.loadDateRx(object:BluJhr.ReceivedData{
+        blue.loadDateRx(object : BluJhr.ReceivedData {
             override fun rxDate(rx: String) {
-                println("------------------- RX $rx --------------------")
-                ejeX+=0.6
-                if (rx.contains("t")){
-                    val date = rx.replace("t","")
+                ejeX += 0.5
+                if (rx.contains("t")) {
+                    val date = rx.replace("t", "")
                     binding.txtTemp.text = "Temperatura: $date°C"
                     temperatura.appendData(DataPoint(ejeX, date.toDouble()), true, 22)
-                }else{
-                    if (rx.contains("h")){
-                        val date = rx.replace("h","")
+                } else {
+                    if (rx.contains("h")) {
+                        val date = rx.replace("h", "")
                         binding.txtPot.text = "Humedad: $date%"
                         humedad.appendData(DataPoint(ejeX, date.toDouble()), true, 22)
                     }
                 }
-
             }
         })
     }
 
     private fun initSweet() {
-        loadSweet = SweetAlertDialog(this,SweetAlertDialog.PROGRESS_TYPE)
-        okSweet = SweetAlertDialog(this,SweetAlertDialog.SUCCESS_TYPE)
-        errorSweet = SweetAlertDialog(this,SweetAlertDialog.ERROR_TYPE)
-        disconnection = SweetAlertDialog(this,SweetAlertDialog.NORMAL_TYPE)
+        loadSweet = SweetAlertDialog(this, SweetAlertDialog.PROGRESS_TYPE)
+        okSweet = SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
+        errorSweet = SweetAlertDialog(this, SweetAlertDialog.ERROR_TYPE)
+        disconnection = SweetAlertDialog(this, SweetAlertDialog.NORMAL_TYPE)
 
         loadSweet.titleText = "Conectando"
         loadSweet.setCancelable(false)
         errorSweet.titleText = "Algo salio mal"
-
         okSweet.titleText = "Conectado"
         disconnection.titleText = "Desconectado"
     }
 
     private fun initGraph() {
-        //permitime controlar los ejes manualmente
         binding.graph.viewport.isXAxisBoundsManual = true;
         binding.graph.viewport.isYAxisBoundsManual = false;
         binding.graph.viewport.setMinX(0.0);
@@ -166,38 +164,23 @@ class MainActivity : AppCompatActivity() {
         binding.graph.viewport.setMaxY(100.0)
         binding.graph.viewport.setMinY(0.0)
 
-        //permite realizar zoom y ajustar posicion eje x
         binding.graph.viewport.isScalable = true
         binding.graph.viewport.setScalableY(true)
 
-
-        // Temperatura DataPoint
         temperatura = LineGraphSeries()
-        //draw points
         temperatura.isDrawDataPoints = true;
-        //draw below points
         temperatura.isDrawBackground = true;
-        //color series
         temperatura.color = Color.RED
 
-
-        // Humedad DataPoint
         humedad = LineGraphSeries()
-        //draw points
         humedad.isDrawDataPoints = true;
-        //draw below points
         humedad.isDrawBackground = true;
-        //color series
         humedad.color = Color.BLUE
-
 
         binding.graph.addSeries(temperatura);
         binding.graph.addSeries(humedad)
     }
 
-    /**
-     * invisible listDevice
-     */
     private fun invisibleListDevice() {
         binding.containerGraph.visibility = View.VISIBLE
         binding.containerDevice.visibility = View.GONE
@@ -205,9 +188,6 @@ class MainActivity : AppCompatActivity() {
         binding.btnViewDevice.text = "DISPOSITIVOS"
     }
 
-    /**
-     * visible list device
-     */
     private fun visibleListDevice() {
         binding.containerGraph.visibility = View.GONE
         binding.containerDevice.visibility = View.VISIBLE
@@ -216,37 +196,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (!blue.stateBluetoooth() && requestCode == 100){
+        if (!blue.stateBluetoooth() && requestCode == 100) {
             blue.initializeBluetooth()
-        }else{
-            if (requestCode == 100){
+        } else {
+            if (requestCode == 100) {
                 devicesBluetooth = blue.deviceBluetooth()
-                if (devicesBluetooth.isNotEmpty()){
-                    val adapter = ArrayAdapter(this,android.R.layout.simple_expandable_list_item_1,devicesBluetooth)
+                if (devicesBluetooth.isNotEmpty()) {
+                    val adapter = ArrayAdapter(
+                        this,
+                        android.R.layout.simple_expandable_list_item_1,
+                        devicesBluetooth
+                    )
                     binding.listDeviceBluetooth.adapter = adapter
-                }else{
-                    Toast.makeText(this, "No tienes vinculados dispositivos", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "No tienes vinculados dispositivos", Toast.LENGTH_SHORT)
+                        .show()
                 }
-
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
     }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        if (blue.checkPermissions(requestCode,grantResults)){
-            Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show()
-            blue.initializeBluetooth()
-        }else{
-            if(Build.VERSION.SDK_INT < Build.VERSION_CODES.S){
-                blue.initializeBluetooth()
-            }else{
-                Toast.makeText(this, "Algo salio mal", Toast.LENGTH_SHORT).show()
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-    }
-
-
-
 }
