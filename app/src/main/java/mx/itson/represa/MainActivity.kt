0fbac527b9c503dc1a355f474a2f57acd2668a74
Diego Castro <diego.castro@potros.itson.edu.mx>
2023-04-26 20:25:54 -0700

    Variables y posible layout

--- app/src/main/java/mx/itson/represa/MainActivity.kt
+++ app/src/main/java/mx/itson/represa/MainActivity.kt
@@ -2,8 +2,37 @@ package mx.itson.represa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import cn.pedant.SweetAlert.SweetAlertDialog
+import com.ingenieriajhr.blujhr.BluJhr
+import com.jjoe64.graphview.series.DataPoint
+import com.jjoe64.graphview.series.LineGraphSeries
 
 class MainActivity : AppCompatActivity() {
+    //bluetooth var
+    lateinit var blue: BluJhr
+    var devicesBluetooth = ArrayList<String>()
+
+    //visible ListView
+    var graphviewVisible = true
+
+    //graphviewSeries
+    lateinit var temperatura: LineGraphSeries<DataPoint?>
+    lateinit var potenciometro:LineGraphSeries<DataPoint>
+
+    //nos indica si estamos recibiendo datos o no
+    var initGraph = false
+    //nos almacena el estado actual de la conexion bluetooth
+    var stateConn = BluJhr.Connected.False
+
+    //valor que se suma al eje x despues de cada actualizacion
+    var ejeX = 0.6
+
+    //sweet alert necesarios
+    lateinit var loadSweet : SweetAlertDialog
+    lateinit var errorSweet : SweetAlertDialog
+    lateinit var okSweet : SweetAlertDialog
+    lateinit var disconnection : SweetAlertDialog
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
